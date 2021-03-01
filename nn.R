d9f78d7b89e216d2da317d9ae5aaa93c9bc1f7ca
Rafa <rafaelaneff@posteo.de>
2021-03-01 16:53:52 +0100

    predicting on test set

--- nn.R
+++ nn.R
@@ -4,6 +4,8 @@ setwd(wd)
 #read data
 train <- read.csv('data/train80.csv')[c(-1)] # exclude ID
 dim(train)
+test <- read.csv('data/test20.csv')[c(-1)] # exclude ID
+dim(test)
 #str(train)
 
 #Change column diagnosis to factor:
@@ -11,41 +13,53 @@ dim(train)
 train$diagnosis <- as.factor(train$diagnosis)
 #levels(ds$diagnosis) <- c('0','1')
 levels(train$diagnosis) <- list("0"="B", "1"="M")
-str(train)
+
+test$diagnosis <- as.factor(test$diagnosis)
+levels(test$diagnosis) <- list("0"="B", "1"="M")
 
 ## Normalize with UDF
 #Custom function for min-max-normalization
 normalize <- function(x) {
   num <- x - min(x)
   denom <- max(x) - min(x)
-  str(min, max)
   return (num/denom)
 }
+#if the data is being normalized we get an ok-ish prediction
+#if not it sucks
 dataset_norm<-as.data.frame(lapply(train[2:31] ,normalize))
-dataset_norm <- cbind(train$diagnosis,dataset_norm)
+dataset_norm <- cbind(diagnosis=train$diagnosis,dataset_norm)
 #str(dataset_norm)
+test_norm<-as.data.frame(lapply(test[2:31] ,normalize))
+test_norm <- cbind(diagnosis=test$diagnosis,dataset_norm)
 
 library(nnet)
 #use one layer in order to compare thee activations in the network 
 #to simpler approaches
-hidden_layers = 10
+hidden_layers = 1
 #fit
+#model overfitting on trainingdata
+#add dropout?
+#loss function?
 NN <-nnet(diagnosis ~. ,
-          data= train,
+          data= dataset_norm,
           size=hidden_layers
 );
-#summary(NN)
+summary(NN)
 #summary(NN$residuals)
 
 prop.table(table(train$diagnosis))
 
 # Predictions on the training set
-nnet_predictions_train <-predict(NN, train, type = "class")
+nnet_predictions_train <-predict(NN, dataset_norm, type = "class")
+nnet_predictions_test <-predict(NN, test_norm, type = "class")
+#table(test$diagnosis, nnet_predictions_test)
 
 # Confusion matrix on training data
 library(caret)
-u <- union(nnet_predictions_train, train$diagnosis)
-t <- table(factor(nnet_predictions_train, u), factor(train$diagnosis, u))
-tbl <- table(train$diagnosis, nnet_predictions_train)
-tbl
+u <- union(nnet_predictions_train, dataset_norm$diagnosis)
+t <- table(factor(nnet_predictions_train, u), factor(dataset_norm$diagnosis, u))
 confusionMatrix(t)   
+
+u_test <- union(nnet_predictions_test, test$diagnosis)
+t_test <- table(factor(nnet_predictions_test, u_test), factor(test$diagnosis, u_test))
+confusionMatrix(t_test)   
