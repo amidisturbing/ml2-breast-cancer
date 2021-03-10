93f3127967585f6f86c34aa95094fb3f8b6dfb6a
Rafa <rafaelaneff@posteo.de>
2021-03-10 15:45:25 +0100

    accuracy above 95%

--- code/nnet.R
+++ code/nnet.R
@@ -8,35 +8,37 @@ library(devtools)
 library(caret)
 library(funModeling)
 library(nnet)
-#library(dplyr)
+library(caTools)
 
 #read data
-train <- read.csv('../data/train80.csv')[c(-1)] # exclude ID
-dim(train)
+ds <- read.csv('../data/train80.csv')[c(-1)] # exclude ID
 test <- read.csv('../data/test20.csv')[c(-1)] # exclude ID
-dim(test)
-#str(train)
 
 #Change column diagnosis to factor:
 #0 for benign, 1 for malignant
-train$diagnosis <- as.factor(train$diagnosis)
-levels(train$diagnosis) <- list("0"="B", "1"="M")
+ds$diagnosis <- as.factor(ds$diagnosis)
+levels(ds$diagnosis) <- list("0"="B", "1"="M")
 
 test$diagnosis <- as.factor(test$diagnosis)
 levels(test$diagnosis) <- list("0"="B", "1"="M")
 
+#create validation split
+# set seed for reproducibility
+set.seed(0)
+train_rows = sample.split(ds$diagnosis, SplitRatio=0.8)
+train = ds[ train_rows,]
+validate  = ds[!train_rows,]
+
+
 # Data Analysis on Training Data
 #shouldn't I do this on the whole dataset (as well)?
-prop.table(table(train$diagnosis))
+prop.table(table(ds$diagnosis))
 #UNCOMMENT following lines for plots and statistics
 #trainingdata_status=df_status(train)
 #plot_num(train)
-corrplot <- cor(train[,2:ncol(train)])
-corrplot(corrplot, order = "hclust", tl.cex = 0.65, addrect = 8)
+
 ## Normalize with UDF
 #Custom function for min-max-normalization
-#NB: Use the scaling function on the training data
-#NOT the whole data
 normalize <- function(x) {
   #DEBUG
   #str(x)
@@ -48,14 +50,18 @@ normalize <- function(x) {
   #str(min, max) #function (..., na.rm = FALSE)  ???
   return (num/denom)
 }
+#applies a min/max-normalization to our dataframe of num-values
+#and returns it together with it'S factor
+apply_normalization <- function(x){
+  norm<-as.data.frame(lapply(x[2:31] ,normalize))
+  #str(norm)
+  norm <- cbind(diagnosis=x$diagnosis,norm)
+}
 #if the data is being normalized we get an ok-ish prediction
 #if not it sucks
-train_norm<-as.data.frame(lapply(train[2:31] ,normalize))
-#str(train_norm)
-train_norm <- cbind(diagnosis=train$diagnosis,train_norm)
-#str(dataset_norm)
-test_norm<-as.data.frame(lapply(test[2:31] ,normalize))
-test_norm <- cbind(diagnosis=test$diagnosis,test_norm)
+train_norm <- apply_normalization(train)
+validate_norm <- apply_normalization(validate)
+test_norm <- apply_normalization(test)
 
 set.seed(42)
 #nnet: size  = number of units in the hidden layer.
@@ -69,24 +75,24 @@ numUnits = 10
 #decay: https://towardsdatascience.com/this-thing-called-weight-decay-a7cd4bcfccab
 NN <-nnet(diagnosis ~. ,
           data = train_norm
-        ,
+          ,
           size = numUnits
 );
 #summary(NN)
 
 NN_1 <-nnet(diagnosis ~. ,
-          data = train_norm
-        ,
-          size = numUnits,
-          decay = 0.1
+            data = train_norm
+            ,
+            size = numUnits,
+            decay = 0.1
 );
 #summary(NN_1)
 
 NN_skip <-nnet(diagnosis ~. ,
-            data = train_norm,
-            size = numUnits,
-            decay = 0.1,
-            skip = TRUE
+               data = train_norm,
+               size = numUnits,
+               decay = 0.1,
+               skip = TRUE
 );
 #summary(NN_skip)
 #summary(NN$residuals)
@@ -97,9 +103,16 @@ model_nnet <- nnet(diagnosis ~ .,
                    rang=0.6,
                    trace=TRUE,maxit=200 )
 
-prop.table(table(test$diagnosis))
+model_nnet_norm <- nnet(diagnosis ~ .,
+                   data=train_norm,
+                   size= 20,
+                   decay= 0.01,
+                   rang=0.6,
+                   trace=TRUE,maxit=2000 )
 
-# Confusion matrix on training data
+prop.table(table(validate$diagnosis))
+
+# Confusion matrix and statistics on training data
 evaluate <- function(pred, ref){
   u <- union(pred, ref)
   t <- table(factor(pred, u), factor(ref, u))
@@ -108,9 +121,9 @@ evaluate <- function(pred, ref){
 
 # Predictions on the training set
 nnet_predictions_train <-predict(NN, train_norm
-                                , type = "class")
+                                 , type = "class")
 evaluate(nnet_predictions_train, train_norm
-        $diagnosis)
+         $diagnosis)
 
 #evaluate best model on NOT normalized training data
 model_nnet_predictions_train <-predict(model_nnet,
@@ -118,6 +131,40 @@ model_nnet_predictions_train <-predict(model_nnet,
                                        type = "class")
 evaluate(model_nnet_predictions_train, train
          $diagnosis)
+
+#------------------------------------------------------------------------------
+# Evaluation on validation data
+#------------------------------------------------------------------------------
+#evaluate best model on NOT normalized validation data
+model_nnet_predictions_validate <-predict(model_nnet, validate, type = "class")
+evaluate(nnet_predictions_validate, validate$diagnosis)
+
+#evaluate best model on normalized validation data - DON'T hold back diagnosis
+model_nnet_predictions_validate_norm <-predict(model_nnet, validate_norm, type = "class")
+evaluate(model_nnet_predictions_validate_norm, validate_norm$diagnosis)
+
+#evaluate best model trained on normalized data, with NOT normalized data
+nnet_predictions_validate_norm <-predict(model_nnet_norm, validate, type = "class")
+evaluate(nnet_predictions_validate_norm, validate_norm$diagnosis)
+
+#evaluate best model trained on normalized data, with  normalized data
+nnet_predictions_validate_norm <-predict(model_nnet_norm, validate_norm, type = "class")
+evaluate(nnet_predictions_validate_norm, validate_norm$diagnosis)
+#----------------------------------------------------------------------------------
+#further evaluation
+nnet_predictions_validate <-predict(NN, validate_norm, type = "class")
+evaluate(nnet_predictions_validate, validate$diagnosis)
+
+#best acchieved accuracy for num_of_units = 1 NN_1 ~81% on test data
+nnet_predictions_test_1 <-predict(NN_1, validate_norm, type = "class")
+evaluate(nnet_predictions_test_1, validate$diagnosis)
+#best acchieved accuracy so far for num_of_units = 1 NN_skip ~83% on test data
+nnet_predictions_test_skip <-predict(NN_skip, validate_norm, type = "class")
+evaluate(nnet_predictions_test_skip, validate$diagnosis)
+
+#------------------------------------------------------------------------------
+# Evaluation on test data
+#------------------------------------------------------------------------------
 #evaluate best model on NOT normalized test data
 nnet_predictions_test <-predict(model_nnet, test, type = "class")
 evaluate(nnet_predictions_test, test$diagnosis)
@@ -142,7 +189,7 @@ nnet_predictions_test_skip <-predict(NN_skip, test_norm, type = "class")
 evaluate(nnet_predictions_test_skip, test$diagnosis)
 
 #import the function from Github
-source_url('https://gist.githubusercontent.com/fawda123/7471137/raw/466c1474d0a505ff044412703516c34f1a4684a5/nnet_plot_update.r')
+#source_url('https://gist.githubusercontent.com/fawda123/7471137/raw/466c1474d0a505ff044412703516c34f1a4684a5/nnet_plot_update.r')
 
 #plot model
-plot.nnet(NN)
+#plot.nnet(NN)
