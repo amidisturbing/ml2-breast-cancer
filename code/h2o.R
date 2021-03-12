e26f0aa85f91af008495a2aa6f000e12221200f0
Rafa <rafaelaneff@posteo.de>
2021-03-12 23:24:27 +0100

    stopped using test set in h20

--- code/h2o.R
+++ code/h2o.R
@@ -2,53 +2,75 @@
 wd = getwd()
 setwd(wd)
 #install.packages("h2o")
+library(caTools)
 
-#Set seed for reproducibility:
+#read data
+ds <- read.csv('../data/train80.csv')[c(-1)] # exclude ID
+library(h2o)
+
+#Change column diagnosis to factor:
+#0 for benign, 1 for malignant
+ds$diagnosis <- as.factor(ds$diagnosis)
+levels(ds$diagnosis) <- list("0"="B", "1"="M")
+
+#create validation split
+# set seed for reproducibility
 set.seed(2)
+train_rows = sample.split(ds$diagnosis, SplitRatio=0.8)
+train = ds[ train_rows,]
+validate  = ds[!train_rows,]
 
 #start a local h2o cluster:
 localH2O = h2o.init(ip="localhost", port = 54321, 
                     startH2O = TRUE, nthreads=-1)
 
-train_h2o <- h2o.importFile("../data/train80.csv")
-test_h2o <- h2o.importFile("../data/test20.csv")
-#test <- read.csv('../data/test20.csv')[c(-1)] # exclude ID
+train_h2o <- as.h2o(train)
+test_h2o <- as.h2o(validate)
 
-train_h2o$diagnosis <- as.factor(train_h2o$diagnosis)
-levels(test_h2o$diagnosis) <- list("0"="B", "1"="M")
+# #Set timer:
+# timer <- proc.time()
+# 
+# #Set Grid parameters:
+# hidden_opt <- list(c(32,32,32), c(5,25,75), c(100,100,100))
+# l1_opt     <- c(1e-5, 1e-4,1e-3)
+# hidden_drpoutRatios <- list(c(0.5,0.5,0.5), c(0.5,0.3,0.2), c(0.1,0.2,0.8))
+# hyper_pars <- list(hidden = hidden_opt, hidden_dropout_ratios = hidden_drpoutRatios, l1 = l1_opt)
+# #Building Grid models:
+# model_grid <- h2o.grid(
+#   algorithm = "deeplearning",
+#   activation = "RectifierWithDropout",
+#   hyper_params = hyper_pars,
+#   x = 3:32,
+#   y = 2,
+#   training_frame = train_h2o,
+#   input_dropout_ratio = 0.2,
+#   balance_classes = T,
+#   momentum_stable = 0.99,
+#   nesterov_accelerated_gradient = T,
+#   epochs = 50,
+#   nfolds = 10,
+#   variable_importances = T,
+#   keep_cross_validation_predictions = T)
+# 
+# for (model_id in model_grid@model_ids) {
+#   auc <- h2o.auc(h2o.getModel(model_id))
+#   print(sprintf('CV set auc: %f', auc))
+# }
 
-test_h2o$diagnosis <- as.factor(test_h2o$diagnosis)
-levels(test_h2o$diagnosis) <- list("0"="B", "1"="M")
-
-#Set timer:
-timer <- proc.time()
-
-#Set Grid parameters:
-hidden_opt <- list(c(32,32,32), c(5,25,75), c(100,100,100))
-l1_opt     <- c(1e-5, 1e-4,1e-3)
-hidden_drpoutRatios <- list(c(0.5,0.5,0.5), c(0.5,0.3,0.2), c(0.1,0.2,0.8))
-hyper_pars <- list(hidden = hidden_opt, hidden_dropout_ratios = hidden_drpoutRatios, l1 = l1_opt)
-#Building Grid models:
-model_grid <- h2o.grid(
-  algorithm = "deeplearning",
-  activation = "RectifierWithDropout",
-  hyper_params = hyper_pars,
-  x = 3:32,
-  y = 2,
-  training_frame = train_h2o,
-  input_dropout_ratio = 0.2,
-  balance_classes = T,
-  momentum_stable = 0.99,
-  nesterov_accelerated_gradient = T,
-  epochs = 50,
-  nfolds = 10,
-  variable_importances = T,
-  keep_cross_validation_predictions = T)
-
-for (model_id in model_grid@model_ids) {
-  auc <- h2o.auc(h2o.getModel(model_id))
-  print(sprintf('CV set auc: %f', auc))
-}
+# Build the first deep learning model, specifying the model_id so you
+# can indicate which model to use when you want to continue training.
+# We will use 4 epochs to start off with and then build an additional
+# 16 epochs with checkpointing.
+dl <- h2o.deeplearning(model_id = 'dl',
+                       x = predictors,
+                       y = target,
+                       training_frame = train,
+                       validation_frame = valid,
+                       distribution = 'multinomial',
+                       epochs = 4,
+                       activation = 'RectifierWithDropout',
+                       hidden_dropout_ratios = c(0, 0),
+                       seed = 1234)
 # # Methods for an H2O model
 # h2o.residual_analysis_plot()
 # h2o.varimp_plot()
