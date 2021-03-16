ae8e0ee9b9da06aba5e9477e292457ccb5796c7c
Rafa <rafaelaneff@posteo.de>
2021-03-16 19:26:56 +0100

    Crossvalidation for model. Nxt step: Choose best model and select by id

--- code/h2o.R
+++ code/h2o.R
@@ -20,10 +20,6 @@ train_rows = sample.split(ds$diagnosis, SplitRatio=0.8)
 train = ds[ train_rows,]
 validate  = ds[!train_rows,]
 
-#TODO:Add Crossvalidation: 
-#stratified:
-#http://docs.h2o.ai/h2o/latest-stable/h2o-docs/data-science/algo-params/fold_assignment.html?highlight=stratified#example
-
 #start a local h2o cluster:
 localH2O = h2o.init(ip="localhost", port = 5321, 
                     startH2O = TRUE, nthreads=-1)
@@ -31,6 +27,13 @@ h2o.removeAll() ## clean slate - just in case the cluster was already running
 
 train_h2o <- as.h2o(train)
 validate_h2o <- as.h2o(validate)
+#load data with h2oand create stratified cross validation split
+test <- h2o.importFile('../data/test20.csv')[c(-1)]
+
+#Change column diagnosis to factor:
+#0 for benign, 1 for malignant
+test $diagnosis <- as.factor(test$diagnosis)
+levels(test$diagnosis) <- list("0"="B", "1"="M")
 
 #load data with h2oand create stratified cross validation split
 training <- h2o.importFile('../data/train80.csv')[c(-1)]
@@ -48,14 +51,14 @@ target = names(train_h2o)[1]
 predictors = names(train_h2o)[2:31]
 
 model <-  h2o.deeplearning(model_id = 'model_cv',
-                           x = 2:31,
+                           x = predictors,
                            y = target,
                            training_frame = training,
                            fold_assignment = assignment_type,
                            distribution = "multinomial",
-                           epochs = 4,
+                           epochs = 500,
                            activation = 'RectifierWithDropout',
-                           nfolds = 5,
+                           nfolds = 10,
                            seed = 1234)
 
 # Build the first deep learning model, specifying the model_id so you
@@ -70,43 +73,44 @@ dl <- h2o.deeplearning(model_id = 'dl',
                        activation = 'RectifierWithDropout',
                        hidden_dropout_ratios = c(0, 0),
                        seed = 1234)
-# #Set timer:
-# timer <- proc.time()
-# 
-# #Set Grid parameters:
-# hidden_opt <- list(c(32,32,32), c(5,25,75), c(100,100,100))
-# l1_opt     <- c(1e-5, 1e-4,1e-3)
-# hidden_drpoutRatios <- list(c(0.5,0.5,0.5), c(0.5,0.3,0.2), c(0.1,0.2,0.8))
-# hyper_pars <- list(hidden = hidden_opt, hidden_dropout_ratios = hidden_drpoutRatios, l1 = l1_opt)
-# #Building Grid models:
-# model_grid <- h2o.grid(
-#   algorithm = "deeplearning",
-#   activation = "RectifierWithDropout",
-#   hyper_params = hyper_pars,
-#   x = 2:31,
-#   y = 1,
-#   training_frame = train_h2o,
-#   input_dropout_ratio = 0.2,
-#   balance_classes = T,
-#   momentum_stable = 0.99,
-#   nesterov_accelerated_gradient = T,
-#   epochs = 50,
-#   nfolds = 10,
-#   variable_importances = T,
-#   keep_cross_validation_predictions = T)
-# 
-# for (model_id in model_grid@model_ids) {
-#   auc <- h2o.auc(h2o.getModel(model_id))
-#   print(sprintf('CV set auc: %f', auc))
-# }
+#Set timer:
+timer <- proc.time()
 
+#Set Grid parameters:
+hidden_opt <- list(c(32,32,32), c(5,25,75), c(100,100,100))
+l1_opt     <- c(1e-5, 1e-4,1e-3)
+hidden_drpoutRatios <- list(c(0.5,0.5,0.5), c(0.5,0.3,0.2), c(0.1,0.2,0.8))
+hyper_pars <- list(hidden = hidden_opt, hidden_dropout_ratios = hidden_drpoutRatios, l1 = l1_opt)
+#Building Grid models:
+model_grid <- h2o.grid(
+  algorithm = "deeplearning",
+  activation = "RectifierWithDropout",
+  hyper_params = hyper_pars,
+  x = predictors,
+  y = target,
+  training_frame = train_h2o,
+  input_dropout_ratio = 0.2,
+  balance_classes = T,
+  momentum_stable = 0.99,
+  nesterov_accelerated_gradient = T,
+  epochs = 500,
+  nfolds = 10,
+  variable_importances = T,
+  keep_cross_validation_predictions = T)
+
+for (model_id in model_grid@model_ids) {
+  auc <- h2o.auc(h2o.getModel(model_id))
+  performance <-  h2o.performance(h2o.getModel(model_id))
+  print(sprintf('CV set auc: %f', auc))
+  print(performance)
+  print("-----------------------------------")
+}
+
+h2o.performance(model)
 # # Methods for an H2O model
-# h2o.residual_analysis_plot()
-# h2o.varimp_plot()
-# h2o.shap_explain_row_plot()
-# h2o.shap_summary_plot()
-# h2o.pd_plot()
-# h2o.ice_plot()
-
+h2o.varimp_plot(model)
+# Create the partial dependence plot
+h2o.pd_plot(model, validate_h2o, column = 'area_se')
+h2o.ice_plot(model, validate_h2o, column = 'texture_worst')
 #shut down H2O instance
 #h2o.shutdown()
